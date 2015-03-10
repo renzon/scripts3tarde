54fac15a2c759204f0b955665461c94ae62b6df5
renzon <renzon@gmail.com>
2015-03-10 17:48:16 -0300

    Herança de Template

--- backend/appengine/routes/usuario.py
+++ backend/appengine/routes/usuario.py
@@ -7,6 +7,12 @@ from gaepermission.decorator import login_not_required
 
 @login_not_required
 @no_csrf
-def index():
-    return TemplateResponse(template_path='home.html')
+def salvar(nome, sobrenome):
+    dct = {'name': nome, 'lastname': sobrenome}
+    return TemplateResponse(dct, '/usuario/ola.html')
 
+
+@login_not_required
+@no_csrf
+def index():
+    return TemplateResponse(template_path='/usuario/usuario_form.html')
