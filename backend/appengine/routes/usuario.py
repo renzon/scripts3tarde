2ac683066f2ac5ebae073fb0ef54922774d79865
renzon <renzon@gmail.com>
2015-03-10 18:20:32 -0300

    If, for e cálculo de rotas com router

--- backend/appengine/routes/usuario.py
+++ backend/appengine/routes/usuario.py
@@ -3,16 +3,24 @@ from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
+from tekton import router
 
 
 @login_not_required
 @no_csrf
 def salvar(nome, sobrenome):
-    dct = {'name': nome, 'lastname': sobrenome}
+    path = router.to_path(index)
+    dct = {'name': nome,
+           'lastname': sobrenome,
+           'usuario_path': path
+    }
     return TemplateResponse(dct, '/usuario/ola.html')
 
 
 @login_not_required
 @no_csrf
 def index():
-    return TemplateResponse(template_path='/usuario/usuario_form.html')
+    path = router.to_path(salvar)
+    ctx = {'usuarios': ['Renzo', 'Jão', 'Paulo', 'Fabiana'],
+           'usuario_path': path}
+    return TemplateResponse(ctx, template_path='/usuario/usuario_form.html')
