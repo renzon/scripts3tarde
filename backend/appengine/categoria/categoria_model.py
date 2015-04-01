4b7aa14648b9050fac567d75ed38f7a3721a0284
renzon <renzon@gmail.com>
2015-04-01 16:32:37 -0300

    Validação com Form

--- backend/appengine/categoria/categoria_model.py
+++ backend/appengine/categoria/categoria_model.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from google.appengine.ext import ndb
+from gaeforms.base import Form, StringField, IntegerField
 
 
 class Categoria(ndb.Model):
     nome = ndb.StringProperty(required=True)
     quantidade = ndb.IntegerProperty(required=True)
+
+class CategoriaForm(Form):
+    nome=StringField(required=True)
+    quantidade=IntegerField(required=True,lower=0)
--- backend/appengine/routes/categorias/new.py
+++ backend/appengine/routes/categorias/new.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from categoria.categoria_model import Categoria
+from categoria.categoria_model import Categoria, CategoriaForm
 from config.template_middleware import TemplateResponse
 from routes import categorias
 from tekton.gae.middleware.redirect import RedirectResponse
 
 
-def salvar(nome, quantidade):
-    erros={}
-    if nome=='':
-        erros['nome']='Campo Obrigatório'
-    try:
-        quantidade = int(quantidade)
-    except:
-        erros['quantidade']='Precisa ser um número inteiro'
+def salvar(**kwargs):
+    form = CategoriaForm(**kwargs)
+    erros=form.validate()
     if not erros:
-        categoria = Categoria(nome=nome, quantidade=int(quantidade))
+        valores_normalizados=form.normalize()
+        categoria = Categoria(**valores_normalizados)
         categoria.put()
         return RedirectResponse(categorias)
     else:
-        valores={'nome':nome,'quantidade':quantidade}
-        ctx={'categoria':valores,'erros':erros}
+        ctx={'categoria':kwargs,'erros':erros}
         return TemplateResponse(ctx,'categorias/categoria_form.html')
