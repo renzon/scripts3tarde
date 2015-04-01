a01c06c0cda31defd9e1ce1536d1de92d1d49ce9
renzon <renzon@gmail.com>
2015-04-01 16:40:57 -0300

    Validação com ModelForm

--- backend/appengine/categoria/categoria_model.py
+++ backend/appengine/categoria/categoria_model.py
@@ -2,12 +2,14 @@
 from __future__ import absolute_import, unicode_literals
 from google.appengine.ext import ndb
 from gaeforms.base import Form, StringField, IntegerField
+from gaeforms.ndb.form import ModelForm
 
 
 class Categoria(ndb.Model):
     nome = ndb.StringProperty(required=True)
     quantidade = ndb.IntegerProperty(required=True)
+    criacao = ndb.DateTimeProperty(auto_now_add=True)
 
-class CategoriaForm(Form):
-    nome=StringField(required=True)
-    quantidade=IntegerField(required=True,lower=0)
+class CategoriaForm(ModelForm):
+    _model_class = Categoria
+    _include = [Categoria.nome, Categoria.quantidade]
